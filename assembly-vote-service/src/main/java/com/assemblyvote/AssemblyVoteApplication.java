fc71578ceac993be256630c8e487f3417a7d1232
gabrielddantas <gabrielddanttas@gmail.com>
2024-07-19 20:57:39 -0300

    initialize project

---
+++ assembly-vote-service/src/main/java/com/assemblyvote/AssemblyVoteApplication.java
@@ -0,0 +1,13 @@
+package com.assemblyvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyVoteApplication.class, args);
+	}
+
+}
---
+++ assembly-vote-service/src/test/java/com/assemblyvote/AssemblyVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assemblyvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
