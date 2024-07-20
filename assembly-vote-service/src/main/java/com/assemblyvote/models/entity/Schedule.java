50689a2f513118b9ef8c84756eb048eb52e1c205
gabrielddantas <gabrielddanttas@gmail.com>
2024-07-19 22:36:00 -0300

    feat: entities and relationship

---
+++ assembly-vote-service/src/main/java/com/assemblyvote/models/entity/Schedule.java
@@ -0,0 +1,26 @@
+package com.assemblyvote.models.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "schedules")
+public class Schedule {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "title", nullable = false)
+  private String title;
+
+  @Column(name = "description", nullable = false)
+  private String description;
+}
---
+++ assembly-vote-service/src/main/java/com/assemblyvote/models/entity/Session.java
@@ -0,0 +1,33 @@
+package com.assemblyvote.models.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "sessions")
+public class Session {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "create_at")
+  private LocalDateTime createAt = LocalDateTime.now();
+
+  @Column(name = "expires_in")
+  private Integer expiresIn = 1;
+
+  @OneToMany
+  @JoinColumn(name = "session_id", nullable = false)
+  private List<Schedule> schedules;
+}
