0dcd3cfe11cdf0de3980541355807846a492b467
gabrielddantas <gabrielddanttas@gmail.com>
2024-07-20 16:06:25 -0300

    fix: create session without expiresIn

--- assembly-vote-service/src/main/java/com/assemblyvote/utils/converters/SessionConverter.java
+++ assembly-vote-service/src/main/java/com/assemblyvote/utils/converters/SessionConverter.java
@@ -4,9 +4,15 @@ import com.assemblyvote.models.dto.SessionDTO;
 import com.assemblyvote.models.entity.Session;
 import com.assemblyvote.utils.DateUtils;
 
+import java.util.Objects;
+
 public class SessionConverter {
 
   public static Session toEntity(SessionDTO sessionDTO) {
+    if (Objects.isNull(sessionDTO) || Objects.isNull(sessionDTO.getExpiresIn())) {
+      return new Session();
+    }
+
     return Session.builder()
         .createAt(DateUtils.nowLocalDateTime())
         .expiresIn(DateUtils.nowPlusMinutesLocalDateTime(sessionDTO.getExpiresIn()))
