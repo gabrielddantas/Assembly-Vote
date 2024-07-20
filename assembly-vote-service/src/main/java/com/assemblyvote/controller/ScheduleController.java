d1eca624367652ffbba5c9c6d504ea4d17f16175
gabrielddantas <gabrielddanttas@gmail.com>
2024-07-20 16:09:32 -0300

    feat: api and service class with yours logics

---
+++ assembly-vote-service/src/main/java/com/assemblyvote/controller/ScheduleController.java
@@ -0,0 +1,54 @@
+package com.assemblyvote.controller;
+
+import com.assemblyvote.exception.ExistsException;
+import com.assemblyvote.exception.NotExistsException;
+import com.assemblyvote.models.dto.ScheduleDTO;
+import com.assemblyvote.models.response.ApiResponse;
+import com.assemblyvote.service.ScheduleService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/schedules")
+public class ScheduleController {
+
+  private final ScheduleService scheduleService;
+
+  @GetMapping()
+  public ResponseEntity<ApiResponse> findAll(@PageableDefault(size = 10) Pageable pageable) {
+    return ResponseEntity.ok(ApiResponse.of(scheduleService.findAll(pageable)));
+  }
+
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ApiResponse> findById(@PathVariable Long id) {
+    try {
+      return ResponseEntity.ok(ApiResponse.of(scheduleService.findById(id)));
+    } catch (NotExistsException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.of(e.getMessage()));
+    }
+  }
+  
+  @PostMapping()
+  public ResponseEntity<ApiResponse> createSchedule(
+      @Valid @RequestBody ScheduleDTO scheduleDTO, BindingResult result) {
+
+    if (result.hasErrors()) {
+      return ResponseEntity.badRequest().body(ApiResponse.fromBindingResult(result));
+    }
+
+    try {
+      return ResponseEntity.status(HttpStatus.CREATED)
+          .body(ApiResponse.of(scheduleService.createSchedule(scheduleDTO)));
+    } catch (ExistsException e) {
+      return ResponseEntity.badRequest().body(ApiResponse.of(e.getMessage()));
+    }
+  }
+}
